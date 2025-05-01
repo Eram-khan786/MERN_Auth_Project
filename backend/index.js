661dbdd2a9191952541fbbb01b2b852206327008
Eram-khan786 <eram22@navgurukul.org>
2025-05-02 01:38:36 +0530

    URL UPDATED

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,7 @@ require('dotenv').config();
 require('./Models/db');
 const PORT = process.env.PORT || 8080;
 
-app.get('/ping', (req, res) => {
-    res.send('PONG');
-});
+
 
 app.use(bodyParser.json());
 app.use(cors());
